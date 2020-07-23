7745e0621805cb162bfd229d465191361101125f
Ariel Sperduti <arielbmx@gmail.com>
2020-07-23 15:33:47 -0300

    Fix

--- purgecss.config.js
+++ purgecss.config.js
@@ -2,11 +2,13 @@
 
 module.exports = {
     // These are the files that Purgecss will search through
-    content: ["_site/**/*.html"],
+    content: ["_site/**/*.html", "_site/**/*.js"],
 
     // These are the stylesheets that will be subjected to the purge
     css: ["_site/assets/css/style.css"],
 
     // File path directory to write purged css file to
-    output: "assets/css"
+    output: "assets/css",
+    fontFace: true,
+    variables: true
 };
